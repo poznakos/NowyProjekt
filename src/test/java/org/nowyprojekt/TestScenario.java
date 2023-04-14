594bcfe77c241c75f9d6fb35f471f0e54b37fbe2
DESKTOP-CE1O4R0\Kk <kpoznakowski@gmail.com>
2023-04-14 14:05:10 +0200

    DodanieChromOptions

--- src/test/java/org/nowyprojekt/TestScenario.java
+++ src/test/java/org/nowyprojekt/TestScenario.java
@@ -1,11 +1,22 @@
 package org.nowyprojekt;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TestScenario {
 
     @Test
     public void someTest(){
+        WebDriverManager.chromedriver().setup();
 
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.komputronik.pl/");
     }
 }
