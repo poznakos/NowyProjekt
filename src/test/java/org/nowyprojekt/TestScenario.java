e68465511bd955ec81b9a8bc95bb7dcb833d6ed1
DESKTOP-CE1O4R0\Kk <kpoznakowski@gmail.com>
2023-04-14 14:27:35 +0200

    PopUpZgadzamSieNaWszystko

--- src/test/java/org/nowyprojekt/TestScenario.java
+++ src/test/java/org/nowyprojekt/TestScenario.java
@@ -2,6 +2,7 @@ package org.nowyprojekt;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,14 +10,17 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class TestScenario {
 
     @Test
-    public void someTest(){
+    public void someTest() {
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications");
 
         WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.komputronik.pl/");
+        By popupZgadzamSieNaWszystko = new By.ByCssSelector("#onetrust-accept-btn-handler");
+        driver.findElement(popupZgadzamSieNaWszystko).click();
     }
 }
